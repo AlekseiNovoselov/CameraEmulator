5df5de6ee7b0009d27a2f0ca69293cfdf206adea
Lexaloris <Lexa-loris@mail.ru>
2016-09-20 20:52:08 +0300

    "эмуляция простого UI камеры "

--- app/src/main/java/com/example/aleksei/cameraemulator/MainActivity.java
+++ app/src/main/java/com/example/aleksei/cameraemulator/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.aleksei.cameraemulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mCameraButton;
+    ImageView mImageBackGround;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraButton = (Button) findViewById(R.id.camera_button);
+        mImageBackGround = (ImageView) findViewById(R.id.image_view);
+        mImageBackGround.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation myFadeInAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.tween);
+                mImageBackGround.startAnimation(myFadeInAnimation);
+            }
+        });
     }
 }
